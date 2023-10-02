8e5b4d8500ad4ee4cb8c854d620eb359c6a34a32
michael-mcinerney <mamcinerney@wpi.edu>
2023-10-02 19:51:42 -0400

    testing suggested server-side optimization

--- server.js
+++ server.js
@@ -1,97 +1,70 @@
-const fs = require('fs');
-const url = require('url');
 const multer = require('multer');
-const path = require('path');
 const lame = require('node-lame').Lame;
+
+const express = require('express');
+const app = express();
 
 // Configure Multer to specify where to store uploaded files
 const storage = multer.memoryStorage(); // Store files in memory
 const upload = multer({ storage: storage });
 
-var buff = null;
-
-const express    = require('express'),
-      app        = express(),
-      audio      = [];
-
-app.use( express.static( 'public' ) );
-app.use( express.static( 'views'  ) );
-app.use( express.json() );
-
+app.use(express.static('public'));
+app.use(express.static('views'));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/upload', upload.single('file'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).send('No file uploaded.');
-  }
+app.post('/upload', upload.single('file'), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).send('No file uploaded.');
+    }
 
-  // The uploaded file is available as req.file
-  const uploadedFile = req.file;
-  const filename = uploadedFile.originalname;
-  const mimetype = uploadedFile.mimetype;
-  const fileBuffer = uploadedFile.buffer; // Access the file data as a Buffer
+    const uploadedFile = req.file;
+    const fileBuffer = uploadedFile.buffer;
 
-  // You can process the file data here
-  // For example, you can send it as a response or save it to a database
-   
-  // Create a Lame decoder instance
-  const decoder = new lame({
-    output: 'buffer',
-  }).setBuffer(fileBuffer);
+    const decoder = new lame({
+      output: 'buffer',
+    }).setBuffer(fileBuffer);
 
-  decoder.decode()
-    .then(() => {
-      // Decoding finished
-      buff = decoder.getBuffer();
-      console.log(buff);
+    await decoder.decode();
 
-      var leftSamples = [];
-      var rightSamples = [];
+    const buff = decoder.getBuffer();
+    const leftSamples = [];
+    const rightSamples = [];
 
-      var offset = 44;
-      while(buff[offset]==0 && buff[offset+1]==0 && buff[offset+2]==0 && buff[offset+3]==0) {
-        offset+=4;
-      } const range = 220500;
-      for(var i = 0; i < range; i++) {
-        // interpret pulse modulation
-        var left1 = buff[offset+(i*4)+0];
-        var left2 = buff[offset+(i*4)+1];
-        var right1 = buff[offset+(i*4)+2];
-        var right2 = buff[offset+(i*4)+3];
+    const offset = 44;
+    const range = 220500;
 
-        var left = left1+(left2*256);
-        var right = right1+(right2*256);
+    for (let i = 0; i < range; i++) {
+      const left = buff[offset + i * 4] + (buff[offset + i * 4 + 1] << 8);
+      const right = buff[offset + i * 4 + 2] + (buff[offset + i * 4 + 3] << 8);
 
-        // convert unsigned bit values to 16 bit signed
-        if(left&0x8000) {
-          left=-((~left&0x7fff)+1);
-        } if(right&0x8000) {
-          right=-((~right&0x7fff)+1);
-        } leftSamples.push(left);
-        rightSamples.push(right);
-      } // Linear interpolation for smoother amplitude values
-      // var interpolationFactor = i / range; // Normalize i to range [0, 1]
-      // leftSamples.push(left * (1 - interpolationFactor) + leftSamples[i - 1] * interpolationFactor);
-      // rightSamples.push(right * (1 - interpolationFactor) + rightSamples[i - 1] * interpolationFactor);
-      const responseObj = {
-        left_samples: leftSamples,
-        right_samples: rightSamples
-      }; res.status(200).json(responseObj); return;
+      // convert unsigned bit values to 16 bit signed
+      if (left & 0x8000) {
+        left = -((~left & 0x7fff) + 1);
+      }
+      if (right & 0x8000) {
+        right = -((~right & 0x7fff) + 1);
+      }
 
-      // graph elements from server
-    })
-    .catch((error) => {
-      // Something went wrong
-      console.log(error);
-    });
+      leftSamples.push(left);
+      rightSamples.push(right);
+    }
+
+    const responseObj = {
+      left_samples: leftSamples,
+      right_samples: rightSamples
+    };
     
-  // res.send(`File uploaded: ${filename}, MIME type: ${mimetype}`);
-
+    res.status(200).json(responseObj);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-app.listen( 3001 )
-
+app.listen(3001);
