bba8e34876b0a1a6d0ea39e5f5a1ae954de1b972
michael-mcinerney <mamcinerney@wpi.edu>
2023-10-02 15:27:52 -0400

    waveform visualization

--- public/index.js
+++ public/index.js
@@ -1,5 +1,34 @@
 const upload = document.getElementById("upload");
 
+const waveformCanvas = document.getElementById('waveformCanvas');
+const context = waveformCanvas.getContext('2d');
+
+// Function to draw the audio waveform
+function drawWaveform(data) {
+    const width = waveformCanvas.width;
+    const height = waveformCanvas.height;
+
+    context.clearRect(0, 0, width, height);
+    context.strokeStyle = 'blue';
+    context.lineWidth = 2;
+
+    context.beginPath();
+
+    const sliceWidth = width / data.length;
+
+    let x = 0;
+    for (let i = 0; i < data.length; i++) {
+        const y = (data[i] / 32768.0) * height;
+        if (i === 0) {
+            context.moveTo(x, y);
+        } else {
+            context.lineTo(x, y);
+        }
+        x += sliceWidth;
+    }
+
+    context.stroke();
+}
 
 // listen for a login action
 upload.onclick = function(event) {
@@ -19,6 +48,7 @@ upload.onclick = function(event) {
         .then( response => { if(response.ok) return response.json() })
         .then( json => {
             console.log(json);
+            drawWaveform(json.left_samples);
         })
         .catch(error => {
             console.error('Error:', error);
