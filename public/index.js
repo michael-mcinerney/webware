d5af2b0e833995bc711c7a09f7bcfc72fe5a16bc
michael-mcinerney <mamcinerney@wpi.edu>
2023-10-02 18:26:48 -0400

    wow this is annoying

--- public/index.js
+++ public/index.js
@@ -21,10 +21,6 @@ const defaultDisplay = playAudio.style.display;
 playAudio.style.display = "none";
 playAudio.addEventListener("click", () => audioPlayback());
 
-const canvas = document.getElementById("waveformCanvas");
-canvas.style.width = window.innerWidth;
-canvas.style.height = 400;
-
 function audioPlayback() {
     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
     const source = audioContext.createBufferSource();
