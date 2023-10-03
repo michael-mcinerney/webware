ba6fab9afa86e4d46837f3d53babe6cd1c7857fa
michael-mcinerney <mamcinerney@wpi.edu>
2023-10-03 17:11:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -193,7 +193,7 @@ app.post('/upload', upload.single('file'), (req, res) => {
       var offset = 44;
       while(buff[offset]==0 && buff[offset+1]==0 && buff[offset+2]==0 && buff[offset+3]==0) {
         offset+=4;
-      } 
+      } start = 0; range = 5;
       for(var i = 0; i < (range*44100); i++) {
         // interpret pulse modulation
         var left1 = buff[offset+(i*4)+0];
